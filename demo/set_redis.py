c7722032a5d3950100bad9cd5e3e145972b03c1f
Andrei Drozdov <sulverus@gmail.com>
2025-03-03 16:30:40 +0100

    review updates

--- demo/set_redis.py
+++ demo/set_redis.py
@@ -2,20 +2,20 @@ import os
 import json
 from redis import Redis
 
-DB_IP = os.getenv('REDIS_HOST', '127.0.0.1')
-DB_PORT = int(os.getenv('REDIS_PORT', '6379'))
-DB_USER = os.getenv('REDIS_USER', 'default')
-DB_PASS = os.getenv('REDIS_PASS', 'test')
-SPACE='vocallout_config'
+REDIS_IP = os.getenv('REDIS_HOST', '127.0.0.1')
+REDIS_PORT = int(os.getenv('REDIS_PORT', '6379'))
+REDIS_USER = os.getenv('REDIS_USER', 'default')
+REDIS_PASS = os.getenv('REDIS_PASS', 'test')
+CONFIG_FIELD='vocallout_config'
 
 def main():
-    db = Redis(host=DB_IP, port=DB_PORT, db=0, username=DB_USER, password=DB_PASS)
+    db = Redis(host=REDIS_IP, port=REDIS_PORT, db=0, username=REDIS_USER, password=REDIS_PASS)
     data = {
         "default": [{"host": "0.0.0.0", "port": 9999}]
     }
-    db.set(SPACE, json.dumps(data))
+    db.set(CONFIG_FIELD, json.dumps(data))
     print('done')
-    print(db.get(SPACE))
+    print(db.get(CONFIG_FIELD))
 
 if __name__ == '__main__':
     main()
